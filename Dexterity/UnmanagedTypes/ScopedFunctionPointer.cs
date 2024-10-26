ec48289586898819b32433339f5882b68a55fe1c
glomdom <22115@uktc-bg.com>
2024-10-26 20:19:10 +0300

    chore: split up `UnmanagedTypes`

---
+++ Dexterity/UnmanagedTypes/ScopedFunctionPointer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dexterity.UnmanagedTypes;
+
+public sealed class UnmanagedString : IDisposable {
+    public nint Ptr { get; }
+
+    public UnmanagedString(string str) {
+        Ptr = Marshal.StringToHGlobalAnsi(str);
+    }
+
+    public void Dispose() {
+        if (Ptr != IntPtr.Zero) {
+            Marshal.FreeHGlobal(Ptr);
+        }
+    }
+
+    public static implicit operator nint(UnmanagedString unmanagedString) => unmanagedString.Ptr;
+}
--- Dexterity/UnmanagedTypes/UnmanagedString.cs
+++ Dexterity/UnmanagedTypes/UnmanagedString.cs
@@ -1,22 +1,11 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Dexterity.UnmanagedTypes;
-
-public sealed class UnmanagedString : IDisposable {
-    public nint Ptr { get; }
-
-    public UnmanagedString(string str) {
-        Ptr = Marshal.StringToHGlobalAnsi(str);
-    }
-
-    public void Dispose() {
-        if (Ptr != IntPtr.Zero) {
-            Marshal.FreeHGlobal(Ptr);
-        }
-    }
-
-    public static implicit operator nint(UnmanagedString unmanagedString) => unmanagedString.Ptr;
-}
 
 public sealed class ScopedFunctionPointer<T> : IDisposable where T : Delegate {
     public nint Ptr { get; }
@@ -28,7 +17,7 @@ public sealed class ScopedFunctionPointer<T> : IDisposable where T : Delegate {
     /// <summary>
     /// Does nothing as function pointers don't need to be EXPLICITLY freed.
     /// </summary>
-    public void Dispose() {}
+    public void Dispose() { }
 
     public static implicit operator nint(ScopedFunctionPointer<T> scopedFunctionPointer) => scopedFunctionPointer.Ptr;
 }
