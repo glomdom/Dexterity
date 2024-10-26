aaba3f1105e2efb0ddf01b897173aab51bbd353d
glomdom <22115@uktc-bg.com>
2024-10-26 20:17:29 +0300

    feat: create `UnmanagedTypes` helpers

--- Dexterity.Sample/Program.cs
+++ Dexterity.Sample/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Runtime.InteropServices;
+
+using Dexterity.UnmanagedTypes;
 using static Dexterity.NativeMethods;
 
 namespace Dexterity.Sample;
@@ -18,43 +20,39 @@ internal static class Program {
 
     public static void Main() {
         var hInstance = Marshal.GetHINSTANCE(typeof(Program).Module);
-        var className = "DexterityMainWindow";
 
-        var wndProcPtr = Marshal.GetFunctionPointerForDelegate((WNDPROC)WindowProc);
-        var classNamePtr = Marshal.StringToHGlobalAnsi(className);
+        using var className = new UnmanagedString("DexterityMainWindow");
+        using var wndProcPtr = new ScopedFunctionPointer<WNDPROC>(WindowProc);
+        using var windowName = new UnmanagedString("Dexterity Sample");
 
         var wc = new WNDCLASSEXA {
             cbSize = (uint)Marshal.SizeOf<WNDCLASSEXA>(),
             lpfnWndProc = wndProcPtr,
             hInstance = hInstance,
-            lpszClassName = classNamePtr,
+            lpszClassName = className,
         };
 
-        try {
-            RegisterClassExA(ref wc);
+        RegisterClassExA(ref wc);
 
-            var hwnd = CreateWindowExA(
-                0,
-                classNamePtr,
-                Marshal.StringToHGlobalAnsi("Dexterity"),
-                WS_OVERLAPPEDWINDOW,
-                CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-                IntPtr.Zero, IntPtr.Zero, hInstance, IntPtr.Zero
-            );
+        var hwnd = CreateWindowExA(
+            0,
+            className,
+            windowName,
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+            IntPtr.Zero, IntPtr.Zero, hInstance, IntPtr.Zero
+        );
 
-            if (hwnd == IntPtr.Zero) {
-                throw new Win32Exception(Marshal.GetLastWin32Error(), "Failed to create window");
-            }
+        if (hwnd == IntPtr.Zero) {
+            throw new Win32Exception(Marshal.GetLastWin32Error(), "Failed to create window");
+        }
 
-            ShowWindow(hwnd, 1);
+        ShowWindow(hwnd, 1);
 
-            var msg = new MSG();
-            while (GetMessage(ref msg, IntPtr.Zero, 0, 0) != false) {
-                TranslateMessage(ref msg);
-                DispatchMessage(ref msg);
-            }
-        } finally {
-            Marshal.FreeHGlobal(classNamePtr);
+        var msg = new MSG();
+        while (GetMessage(ref msg, IntPtr.Zero, 0, 0) != false) {
+            TranslateMessage(ref msg);
+            DispatchMessage(ref msg);
         }
     }
 }
---
+++ Dexterity/UnmanagedTypes.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Dexterity.UnmanagedTypes;
+
+public sealed class UnmanagedString : IDisposable {
+    public nint Ptr { get; }
+
+    public UnmanagedString(string str) {
+        Ptr = Marshal.StringToHGlobalAnsi(str);
+    }
+
+    public void Dispose() {
+        if (Ptr != IntPtr.Zero) {
+            Marshal.FreeHGlobal(Ptr);
+        }
+    }
+
+    public static implicit operator nint(UnmanagedString unmanagedString) => unmanagedString.Ptr;
+}
+
+public sealed class ScopedFunctionPointer<T> : IDisposable where T : Delegate {
+    public nint Ptr { get; }
+
+    public ScopedFunctionPointer(T del) {
+        Ptr = Marshal.GetFunctionPointerForDelegate(del);
+    }
+
+    /// <summary>
+    /// Does nothing as function pointers don't need to be EXPLICITLY freed.
+    /// </summary>
+    public void Dispose() {}
+
+    public static implicit operator nint(ScopedFunctionPointer<T> scopedFunctionPointer) => scopedFunctionPointer.Ptr;
+}
